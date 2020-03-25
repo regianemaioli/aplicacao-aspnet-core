78f02f69ef70c715a2a17de9054e022efeb0cf41
Regiane Maioli <regianemaioli@hotmail.com>
2020-03-25 15:10:24 -0300

    Criando relatorio de livros

---
+++ Startup.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace web
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            var livros = new List<Livro>();
+            livros.Add(new Livro("001", "Quem mexeu na minha query?", 12.99m));
+            livros.Add(new Livro("002", "Fique rico com C#", 30.99m));
+            livros.Add(new Livro("003", "Java para baixinhos", 25.99m));
+
+            app.Run(async (context) =>
+            {
+                foreach(var livro in livros)
+                {
+                    await context.Response.WriteAsync($"{livro.Codigo,-10}{livro.Nome,-40}{livro.Preco,10}\r\n");
+                }
+               
+            });
+        }
+    }
+}
